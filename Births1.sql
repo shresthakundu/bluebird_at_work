263cdd93ad2a4cb6d9e2f82e28bf739094d190fd
shresthakundu <100848496+shresthakundu@users.noreply.github.com>
2022-03-05 20:53:29 +0530

    Update Births1.sql

--- Births1.sql
+++ Births1.sql
@@ -1,3 +1,5 @@
+--What is the average number of births in total?
+
 SELECT avg(Births) 
 FROM `bigquery-public-data.sdoh_cdc_wonder_natality.county_natality` 
---The avaerage number of births in the counties is 6173.102.
+--The average number of births in the counties is 6173.102.
