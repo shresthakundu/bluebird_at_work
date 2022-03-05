3433de427ff78f5c9aa4421e56794b67a21d780e
shresthakundu <100848496+shresthakundu@users.noreply.github.com>
2022-03-05 20:12:16 +0530

    Create Births2.sql

---
+++ Births2.sql
@@ -0,0 +1,5 @@
+SELECT (Congenital_Abnormality_Checked_Desc) AS CACD, COUNT(Congenital_Abnormality_Checked_Desc) AS Total_Congen_Abnormalities
+FROM `bigquery-public-data.sdoh_cdc_wonder_natality.county_natality_by_congenital_abnormalities` 
+group by 1
+Order by 2 desc 
+--Out of the entire population, 32.5% of the population has been checked with atleast one form of congenital abnormalities.
