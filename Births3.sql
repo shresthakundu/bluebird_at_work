15918b4d26707696a215b92a02f7b2c0202a7062
shresthakundu <100848496+shresthakundu@users.noreply.github.com>
2022-03-05 20:16:07 +0530

    Create Births3.sql

---
+++ Births3.sql
@@ -0,0 +1,13 @@
+SELECT (Congenital_Abnormality_Checked_Desc) AS CACD, COUNT(Congenital_Abnormality_Checked_Desc) AS Total_Congen_Abnormalities
+FROM `bigquery-public-data.sdoh_cdc_wonder_natality.county_natality_by_congenital_abnormalities` 
+where Ave_Age_of_Mother between 23.73 and 30
+group by 1
+Order by 2 desc 
+
+SELECT (Congenital_Abnormality_Checked_Desc) AS CACD, COUNT(Congenital_Abnormality_Checked_Desc) AS Total_Congen_Abnormalities
+FROM `bigquery-public-data.sdoh_cdc_wonder_natality.county_natality_by_congenital_abnormalities` 
+where Ave_Age_of_Mother between 30.1 and 35.71
+group by 1
+Order by 2 desc 
+
+--Mothers in the age range 23.73 to 30 years of age observe higher number of congenital abnormlities in the children than mothers in the age range 30.1 to 35.71 years of age.
