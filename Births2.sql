0c65e0d9833a7cccce88d4e9a63438327c294b20
shresthakundu <100848496+shresthakundu@users.noreply.github.com>
2022-03-05 20:54:39 +0530

    Update Births2.sql

--- Births2.sql
+++ Births2.sql
@@ -1,3 +1,4 @@
+--What is the percentage of congenital abnormalities checked among the given population?
 SELECT (Congenital_Abnormality_Checked_Desc) AS CACD, COUNT(Congenital_Abnormality_Checked_Desc) AS Total_Congen_Abnormalities
 FROM `bigquery-public-data.sdoh_cdc_wonder_natality.county_natality_by_congenital_abnormalities` 
 group by 1
