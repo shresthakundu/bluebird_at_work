fa287794f1b1b265420225bda67fee9bf9cd7fe3
shresthakundu <100848496+shresthakundu@users.noreply.github.com>
2022-03-05 21:02:53 +0530

    Update Births4.sql

--- Births4.sql
+++ Births4.sql
@@ -1,5 +1,7 @@
+--What is the distibution of types of payments among the population?
+
 select Source_of_Payment, count(Source_of_Payment) as Number_of_payments
 from `bigquery-public-data.sdoh_cdc_wonder_natality.county_natality_by_payment`
 group by Source_of_Payment
 
-The distribution among payment options are almost similar. They are all within 22%-24% of the distribution.
+--The distribution among payment options are almost similar. They are all within 22%-24% of the distribution.
